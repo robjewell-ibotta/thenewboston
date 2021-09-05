7b57fb42559fa4347fc0ce93fb142818a0266daa
Rob Jewell <rob.jewell@ibotta.com>
2021-09-05 12:12:10 -0400

    configure mockk to not evaluate what is
    returned from method expectation

--- src/test/kotlin/tv/codealong/tutorials/springboot/thenewboston/BankServiceTest.kt
+++ src/test/kotlin/tv/codealong/tutorials/springboot/thenewboston/BankServiceTest.kt
@@ -10,17 +10,14 @@ import tv.codealong.tutorials.springboot.thenewboston.datasource.BankDataSource
 
 internal class BankServiceTest {
 
-  private val dataSource: BankDataSource = mockk()
+  private val dataSource: BankDataSource = mockk(relaxed = true)
 
   private val bankService = BankService(dataSource)
 
   @Test
   fun `should call its data source to retrieve banks`() {
-    // given
-    every { dataSource.retrieveBanks() } returns emptyList()
 
     // when
-    // val banks = bankService.getBanks()
     bankService.getBanks()
 
     // then
